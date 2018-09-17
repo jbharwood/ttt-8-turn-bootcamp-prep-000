77245881af9fffce85ea18902b1af9a7472ccb60
Joseph Harwood <josephblakeharwood@gmail.com>
2018-09-17 21:35:07 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,18 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input) 
+  input.to_i - 1
+end
+
+def move(array, index, value = "X")
+  array[index] = value
+end
 def valid_move?(board, index)
   index = index.to_i
   if index.between?(0, 8) == true && !position_taken?(board, index)
